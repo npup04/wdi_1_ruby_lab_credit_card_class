988c46723fb4f2df0fa9eb8ef51cda25e7d9be7e
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-01-15 23:17:51 -0500

    Updated exercises, still working on expiration_date method to check if exp date is in the future

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,2 +1,48 @@
+require "pry"
+require 'date'
+
 class CreditCard
+  def initialize(card_type, name, expiration, ccv, zipcode, card_number)
+    @card_type = card_type
+    @name = name
+    @expiration = expiration
+    @ccv = ccv
+    @zipcode = zipcode
+    @card_number = card_number
+  end
+
+  def card_type
+    @card_type
+  end
+
+  def name
+    @name.length
+  end
+
+  def expiration
+    @expiration.length
+  end
+
+  def ccv
+    @ccv.length
+  end
+
+  def zipcode
+    @zipcode.length
+  end
+
+  def card_number
+    @card_number.length
+  end
+
+  def expiration_date?
+    current_year = Date.today.strftime("%y").to_i
+    current_month = Date.today.strftime("%m").to_i
+    exp_future_year = 14
+    exp_future_month = 03
+
+    exp_future_year > current_year || (exp_future_year < current_year && exp_future_month > current_month)
+  end
 end
+# binding.pry
+# puts "---"
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -1,1 +1,76 @@
 require_relative '../lib/credit_card'
+
+# Note: We put the name of the class here instead of a string
+RSpec.describe CreditCard do
+  # We use `let` for setting local variables
+  let(:card_type) { "Mastercard" }
+  let(:name) { "Jason Wharff" }
+  let(:expiration) { "0316" }
+  let(:ccv) { "123" }
+  let(:zipcode) { "02169" }
+  let(:card_number) { "5555555555554444" }
+
+  # Similarly, we use `subject` to create an instance
+  # of the main object we are testing
+  subject(:credit_card) { CreditCard.new(card_type, name, expiration, ccv, zipcode, card_number)}
+
+  # Test the initialize method
+  # For this one, we mostly check that it creates a new credit card
+  describe '#initialize' do
+  it 'creates a new instance of CreditCard' do
+    expect(credit_card).to be_a CreditCard
+    end
+  end
+
+  # Test that the credit card is an accepted card type
+  describe '#card_type' do
+  it 'tests the credit card type' do
+    expect(credit_card.card_type).to be card_type
+    end
+  end
+
+  # Test name length
+  describe '#name' do
+  it 'tests that the credit card has a name length between 1 - 20 ' do
+    expect(credit_card.name).to be_between(1, 20).inclusive
+    end
+  end
+
+  # Test expiration length
+  describe '#expiration' do
+  it 'tests that the credit card has an expiration length of 4' do
+    expect(credit_card.expiration).to be 4
+    end
+  end
+
+  # Test the ccv length
+  describe '#ccv' do
+  it 'tests that the credit card has a ccv length of 3' do
+    expect(credit_card.ccv).to be 3
+    end
+  end
+
+  # Test the zipcode length
+  describe '#zipcode' do
+  it 'tests that the credit card has a zipcode length of 5' do
+    expect(credit_card.zipcode).to be 5
+    end
+  end
+
+  # Test the card_number length
+  describe '#card_number' do
+  it 'tests that the credit card has a card_number length of 16' do
+    expect(credit_card.card_number).to be 16
+    end
+  end
+
+  # Test if expiration date is in the future
+  describe '#expiration_date?' do
+    subject(:expiration_date) { CreditCard.new(card_type, name, expiration, ccv, zipcode, card_number)}
+    it 'returns if the expiration_date is in the future' do
+      # expect(credit_card.expiration_date?).to be false
+      expect(credit_card.expiration_date?).to be false
+    end
+  end
+
+end
